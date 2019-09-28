4e75260a46777c4f5179c5f636c049e71b9545c8
Abdoul Diallo <abdouldiallo@Abdouls-MacBook-Pro.local>
2019-09-27 20:12:27 -0500

    second

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import Hero from '../Components/Hero';
+import {Link} from 'react-router-dom';
+import Banner from '../Components/Banner';
 
 export default function Error() {
-    return (<Hero />);
+    return (<Hero>
+        <Banner title='404' subtitle='Page Not Found'>
+            <Link to='/' className='btn-primary'>Back To Home</Link>
+        </Banner>
+    </Hero>);
 }
 
